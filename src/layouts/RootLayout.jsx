218e413db6c32ea719efe323ce319160d29c3eca
sujan khatri <sujansince2059@gmail.com>
2023-12-14 20:47:47 +0545

    responsive design

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -5,10 +5,16 @@ import Nav from "../../Components/Nav";
 export default function RootLayout() {
   return (
     <Grid templateColumns={"repeat(6,1fr)"} bg="gray.50">
-      <GridItem as={"aside"} colSpan={1} bg="purple.400" minHeight={"100vh"}>
+      <GridItem
+        as={"aside"}
+        colSpan={{ base: 6, lg: 2, xl: 1 }}
+        bg="purple.400"
+        minHeight={{ lg: "100vh" }}
+        p={{ base: "20px", lg: "30px" }}
+      >
         <span>sidebar</span>
       </GridItem>
-      <GridItem as={"main"} colSpan={5} p={"40px"}>
+      <GridItem as={"main"} colSpan={{ base: 6, lg: 4, xl: 5 }} p={"40px"}>
         <Nav />
         <Outlet />
       </GridItem>
