b48ca24ca95c21d0fe672c078f9750cc58a51b91
sujan khatri <sujansince2059@gmail.com>
2023-12-14 17:57:38 +0545

    component and style props

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from "@chakra-ui/react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,36 @@
+import { Heading, Text, Container, Box } from "@chakra-ui/react";
 export default function Dashboard() {
+  const styleBox = {
+    p: "20px",
+    bg: "yellow.500",
+    color: "white",
+    m: "30px",
+    fontWeight: "bold",
+    // for hovering or pseudo styling
+    ":hover": {
+      color: "black",
+      backgroundColor: "pink.300",
+    },
+  };
   return (
-    <div>Dashboard</div>
-  )
+    <>
+      <Container as="section">
+        <Heading m="20px">chakara Ui is fun</Heading> {/*applies H2*/}
+        <Text>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat
+          reiciendis adipisci pariatur praesentium voluptatum placeat nulla
+          accusantium numquam vero soluta.
+        </Text>{" "}
+        {/*p tag */}
+      </Container>
+      {/* contents wrapped inside container is centered */}
+      <Box m="20px" as="p">
+        hello world
+      </Box>
+      <Box sx={styleBox}>this is styled with objects</Box>
+      <Box sx={{ backgroundColor: "blue", color: "white", padding: "20px" }}>
+        This box has custom styles applied using sx prop.
+      </Box>
+    </>
+  );
 }
