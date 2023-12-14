94da90f791167a897d31465ec7ed9ae3ede179ea
sujan khatri <sujansince2059@gmail.com>
2023-12-14 18:43:54 +0545

    build the nav

---
+++ Components/Nav.jsx
@@ -0,0 +1,31 @@
+import {
+  Flex,
+  Heading,
+  Spacer,
+  Box,
+  Text,
+  HStack,
+  Button,
+} from "@chakra-ui/react";
+
+function Nav() {
+  return (
+    <>
+      <Flex as={"nav"} p={"10px"} alignItems="center">
+        <Heading as={"h1"}>Suzan Codes</Heading>
+        <Spacer />
+        <HStack spacing="10px">
+          <Box p={"10px"} bg="gray.200">
+            S
+          </Box>
+          <Text>sujansince2059@gmail.com</Text>
+          <Button colorScheme="purple" fontWeight={"bold"}>
+            LogOut
+          </Button>
+        </HStack>
+      </Flex>
+    </>
+  );
+}
+
+export default Nav;
--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,9 +1,11 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom";
+import Nav from "../../Components/Nav";
 
 export default function RootLayout() {
   return (
     <div>
+      <Nav />
       <Outlet />
     </div>
-  )
+  );
 }
