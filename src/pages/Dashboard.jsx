ba6ee25f26cebdedbf5ca2b307aa1cd18af86a31
sujan khatri <sujansince2059@gmail.com>
2023-12-14 20:31:47 +0545

    dashboard grid layout with simpleGrid

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,36 +1,54 @@
-import { Heading, Text, Container, Box } from "@chakra-ui/react";
+import { Heading, Text, Container, Box, SimpleGrid } from "@chakra-ui/react";
 export default function Dashboard() {
-  const styleBox = {
-    p: "20px",
-    bg: "yellow.500",
-    color: "white",
-    m: "30px",
-    fontWeight: "bold",
-    // for hovering or pseudo styling
-    ":hover": {
-      color: "black",
-      backgroundColor: "pink.300",
-    },
-  };
+  // const styleBox = {
+  //   p: "20px",
+  //   bg: "yellow.500",
+  //   color: "white",
+  //   m: "30px",
+  //   fontWeight: "bold",
+  //   // for hovering or pseudo styling
+  //   ":hover": {
+  //     color: "black",
+  //     backgroundColor: "pink.300",
+  //   },
+  // };
   return (
     <>
+      <SimpleGrid spacing={10} p={"10px"} minChildWidth={"250px"}>
+        <Box bg="white" h="200px" border={"1px solid black"}></Box>
+        <Box bg="white" h="200px" border={"1px solid black"}></Box>
+        <Box bg="white" h="200px" border={"1px solid black"}></Box>
+        <Box bg="white" h="200px" border={"1px solid black"}></Box>
+
+        <Box bg="white" h="200px" border={"1px solid black"}></Box>
+        <Box bg="white" h="200px" border={"1px solid black"}></Box>
+        <Box bg="white" h="200px" border={"1px solid black"}></Box>
+        <Box bg="white" h="200px" border={"1px solid black"}></Box>
+
+        <Box bg="white" h="200px" border={"1px solid black"}></Box>
+        <Box bg="white" h="200px" border={"1px solid black"}></Box>
+        <Box bg="white" h="200px" border={"1px solid black"}></Box>
+        <Box bg="white" h="200px" border={"1px solid black"}></Box>
+      </SimpleGrid>
+      {/* 
+      ****** this is not related to this project just for learning ****
       <Container as="section">
-        <Heading m="20px">chakara Ui is fun</Heading> {/*applies H2*/}
+        <Heading m="20px">chakara Ui is fun</Heading> applies H2
         <Text>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat
           reiciendis adipisci pariatur praesentium voluptatum placeat nulla
           accusantium numquam vero soluta.
         </Text>{" "}
-        {/*p tag */}
+        p tag
       </Container>
-      {/* contents wrapped inside container is centered */}
+      contents wrapped inside container is centered
       <Box m="20px" as="p">
         hello world
       </Box>
       <Box sx={styleBox}>this is styled with objects</Box>
       <Box sx={{ backgroundColor: "blue", color: "white", padding: "20px" }}>
         This box has custom styles applied using sx prop.
-      </Box>
+      </Box> */}
     </>
   );
 }
